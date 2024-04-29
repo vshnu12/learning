91d4b468a5d4d088fda8cf92b2d3371658b38784
vishn <vishnuhemaraj2002@gmail.com>
2024-04-29 22:12:15 +0530

    initial commit

---
+++ samblenavigation/index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/screens/app';
+import {name as appName} from './app.json';
+//import loginScreen from './src/screens/home';
+
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ samblenavigation/styles.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+
+export const styles =StyleSheet.create({
+outerCon:{
+padding: 15,
+margin: 15,
+},
+textStyle: {
+fontSize: 40,
+textAlign: 'left',
+fontWeight: 'bold',
+color: 'green',
+},
+textNewStyle: {color: 'red', fontSize: 12},
+container: {
+flex: 1,
+paddingTop: 25,
+marginHorizontal: 16,
+},
+item: {
+backgroundColor: '#f9c2ff',
+padding: 20,
+marginVertical: 8,
+},
+header: {
+fontSize: 32,
+backgroundColor: '#fff',
+},
+title: {
+fontSize: 24,
+},
+});
